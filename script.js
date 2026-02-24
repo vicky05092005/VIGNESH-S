bda4fe9f71e13f291d96d8bf2a23beb6ddc464e0
vicky05092005 <23ai006@amjaincollege.edu.in>
2026-02-24 13:55:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const products = [
     { name: "Coffee Culture", price: 699, oldPrice: 899, badge: "Bestseller", img: "Mask group.png" },
     { name: "Elegant Essentials", price: 299, badge: "Limited Stock", img: "Mask group-1.png" },
-    { name: "Warm Scarves", price: 399, img: "images/Mask group-2.png" },
+    { name: "Warm Scarves", price: 399, img: "Mask group-2.png" },
     { name: "Timeless Ticking", price: 899, oldPrice: 1199, badge: "Customizable", img: "Mask group-3.png" },
     { name: "Coffee Culture", price: 699, oldPrice: 899, badge: "Bestseller", img: "Mask group-4.png" },
     { name: "Elegant Essentials", price: 299, badge: "Limited Stock", img: "Mask group-5.png" },
@@ -28,4 +28,5 @@ products.forEach(product => {
     `;
 
     container.appendChild(card);
+
 });
